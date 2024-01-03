8a1024a424eb1c4ad18a4151e1fcc1e8f3705f3c
Kemmie001 <oluwakemisola757@gmail.com>
2024-01-03 17:33:21 +0100

    ft: fetch vehicle from backend

--- src/views/Vehicle/index.vue
+++ src/views/Vehicle/index.vue
@@ -1,15 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, onMounted, ref } from "vue";
+import { userInfo } from "@/store/user";
+const vehicleData = computed(() => {
+  return userInfo().vehicleData;
+});
+const roller = ref(false);
+
+onMounted(async () => {
+  if (!Object.keys(vehicleData.value).length) {
+    roller.value = true;
+    await userInfo().fetchUserVehicle();
+    roller.value = false;
+  }
+});
+</script>
 
 <template>
   <div class="flex px-8 flex-col md:flex-row gap-10 my-10">
     <div>
       <h1 class="mb-5 font-bold">Vehicle Details</h1>
+      <div
+        v-if="roller"
+        class="animate-spin roller flex items-center justify-center"
+      ></div>
       <div>
         <img src="../../assets/img/car.png" alt="car" />
       </div>
     </div>
     <div class="my-10">
-      <p class="text-sm font-normal">Mercedes</p>
+      <p class="text-sm font-normal">{{ vehicleData.brand }}</p>
       <h1 class="mb-8 font-bold text-[32px]">2024 Mercedes Benz GLC</h1>
       <div class="mt-4 text-base">
         <span class="flex items-center gap-3 pb-4">
@@ -22,15 +41,19 @@
         </span>
         <span class="flex items-center gap-3 pb-4">
           <p class="text-sm">Plate Number:</p>
-          <p class="text-lg font-semibold">AKR-207XY34</p>
+          <p class="text-lg font-semibold">{{ vehicleData.plate_no }}</p>
         </span>
         <span class="flex items-center gap-3 pb-4">
           <p class="text-sm">Engine Number:</p>
-          <p class="text-lg font-semibold">PJ6547207XY34</p>
+          <p class="text-lg font-semibold">{{ vehicleData.engine_no }}</p>
         </span>
         <span class="flex items-center gap-3 pb-4">
           <p class="text-sm">Chassis Number:</p>
           <p class="text-lg font-semibold">SV30-01692</p>
+        </span>
+        <span class="flex items-center gap-3 pb-4">
+          <p class="text-sm">Vehicle Type:</p>
+          <p class="text-lg font-semibold">{{ vehicleData.vehicle_type }}</p>
         </span>
         <span class="flex items-center gap-3 pb-4">
           <p class="text-sm">Fuel Type:</p>
